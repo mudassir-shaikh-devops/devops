327b756e81e557a62868364730871cb19256dd71
Shaikh Mudassir <shaikh09mudassir@gmail.com>
2026-08-03 17:59:19 +0400

    Updated Files

---
+++ Pyhton+Docker-Project/Project01/p1.py
@@ -0,0 +1,7 @@
+print("Performing sum of numbers \n")
+
+Num1=int(input("Enter first number: "))
+Num2=int(input("Enter second number: "))
+
+result=Num1+Num2
+print("The sum of", Num1, "and", Num2, "is:", result)
